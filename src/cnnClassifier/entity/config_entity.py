57cdb79844db4b77c061dc9e35b8eb3c8ea6eec3
EyaBouajila <ebouajila20001304@gmail.com>
2023-11-29 15:16:38 +0100

    data ingestion completed

---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,12 @@
+# Update the entity
+# entity = returnType of a function
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
