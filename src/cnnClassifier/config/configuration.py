f56373b6b41b4c32b7377e136837b7504b6f23c0
EyaBouajila <ebouajila20001304@gmail.com>
2023-11-29 18:19:30 +0100

    training added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,8 +3,7 @@ from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
                                                 PrepareCallbacksConfig,
-                                                TrainingConfig,
-                                                EvaluationConfig)
+                                                TrainingConfig)
 
 
 # Update the configuration manager in src config
@@ -55,7 +54,7 @@ class ConfigurationManager:
 
     def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
         config = self.config.prepare_callbacks
-        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        model_ckpt_dir = os.path.dirname(str(config.checkpoint_model_filepath))
         create_directories([
             Path(model_ckpt_dir),
             Path(config.tensorboard_root_log_dir)
@@ -68,3 +67,26 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+    
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "GalaxyData")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
